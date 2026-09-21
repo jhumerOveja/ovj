b2852d07679d8ce30e1dbed6958e376e4a60d93d
jhumerOveja <jhumerali2@gmail.com>
2026-09-21 14:24:25 -0400

    Actualizar config.js

--- config.js
+++ config.js
@@ -17,7 +17,7 @@
 
 window.OVJ_CONFIG = {
     // 🔥 Cambiar esta URL cada vez que reinicies el túnel
-    urlTunel: 'https://surround-import-checks-campaigns.trycloudflare.com',
+    urlTunel: 'https://bare-edmonton-mesa-click.trycloudflare.com',
 
     // WhatsApp de publicidad (no cambia)
     whatsappPublicidad: '59160004962',
