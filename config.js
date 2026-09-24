37285389ac6990c73703a0725b0f5d2f0def4cea
jhumerOveja <jhumerali2@gmail.com>
2026-09-24 12:20:38 -0400

    Actualizar config.js

--- config.js
+++ config.js
@@ -17,7 +17,7 @@
 
 window.OVJ_CONFIG = {
     // 🔥 Cambiar esta URL cada vez que reinicies el túnel
-    urlTunel: 'https://harbour-transactions-drew-americas.trycloudflare.com',
+    urlTunel: 'https://sense-vice-names-acceptance.trycloudflare.com',
 
     // WhatsApp de publicidad (no cambia)
     whatsappPublicidad: '59160004962',
