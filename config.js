b15cd55f6fe80b233953f647e81cd4032ab025a6
jhumerOveja <jhumerali2@gmail.com>
2026-09-25 11:52:37 -0400

    Actualizar config.js

--- config.js
+++ config.js
@@ -17,7 +17,7 @@
 
 window.OVJ_CONFIG = {
     // 🔥 Cambiar esta URL cada vez que reinicies el túnel
-    urlTunel: 'https://subscriber-skirt-casinos-acdbentity.trycloudflare.com',
+    urlTunel: 'https://bytes-referring-bishop-collectibles.trycloudflare.com',
 
     // WhatsApp de publicidad (no cambia)
     whatsappPublicidad: '59160004962',
